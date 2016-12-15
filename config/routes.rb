93a243a5074fe86ff3a85c53ebedc86bb0d26507
Sosho Chang <soshochang@gmail.com>
2016-12-14 23:25:08 -0800

    add sessions controller for testing swap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/destroy'
+
+  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
